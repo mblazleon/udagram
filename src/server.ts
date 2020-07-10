4bc948fbde0bb045cd799f2ea3c21680740657f7
ablazleon <ablazleon@gmail.com>
2020-07-11 00:20:12 +0200

    Server filled but not working

--- src/server.ts
+++ src/server.ts
@@ -13,15 +13,47 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
+  app.get('/filteredimage', async (req: Request, res: Response) => {
+    const url = req.query.image_url;
   //    1. validate the image_url query
+
+    if(!url){
+        return res.status(400).send({message: 'Image url is required'})
+    }
+
   //    2. call filterImageFromURL(image_url) to filter the image
+  // Wait until the promise is filtered.
+    const urlFilteredImage = await filterImageFromURL(url);
+
   //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
+  // As the method deleteLocalFiles() needs an Array<string> to unsync each local path, the path is first send and then stored in an array
+
+  res.sendFile(urlFilteredImage, (err) => {
+    deleteLocalFiles(urlFilteredImage);
+    if(err){
+        console.log(err);
+    }
+
+   //   4. deletes any files on the server on finish of the response
+
+
+  } )
+
+  });
+
+
+
+
+
+
+
+
+
+
   // QUERY PARAMATERS
   //    image_url: URL of a publicly accessible image
   // RETURNS
@@ -29,7 +61,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
-  //! END @TODO1
+  //! END
   
   // Root Endpoint
   // Displays a simple message to the user
